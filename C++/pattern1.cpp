233925fcf0650a7b740b2dc2538e35d936588768
imrahulpal1 <rahulpal14093@gmail.com>
2025-01-12 11:08:14 +0530

    Add files via upload

---
+++ C++/pattern1.cpp
@@ -0,0 +1,23 @@
+/*1 2 3 4 
+  1 2 3 4 
+  1 2 3 4    make this pattern */ 
+  #include<iostream>
+  using namespace std;
+
+  int main(){
+    int n,i=1;
+    cin>>n;
+    while(i<=n){
+       int j=1;
+        while(j<=n){
+            cout<<j;
+            j=j+1;
+            
+        }
+        cout<<endl;
+        
+        i=i+1;
+    }
+  }
+ 
+ 
